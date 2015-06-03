b1288cd3f0769d142c72e62e65023b8fede95d89
aaruizcarrera <aaruiz@outlook.es>
2015-06-02 21:25:16 -0500

    Agregado constructor

--- negocios/negoProyecto.php
+++ negocios/negoProyecto.php
@@ -1,10 +1,30 @@
 <?php
+	include_once '../datos/config.php';
 	include_once 'negoArea.php';
 
 	class NegoProyecto{
 		private $id;
 		private $nombre;
 		private $area;
+
+		function __construct($id=null,$nombre=null,$area=null){
+
+			$parametros = get_defined_vars();
+			$variables = get_class_vars(__CLASS__);
+			
+			foreach ($parametros as $nombre_parametro => $valor) {
+				if($valor == null)
+					break;
+				else{
+					foreach ($variables as $nombre_variable => $default) {				
+						if($nombre_variable==$nombre_parametro){
+							$this->$nombre_variable = $valor;
+							break;
+						}
+					}
+				}
+			}			
+		}
 	    
 	    public function getId(){
 	        return $this->id;
